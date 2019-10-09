82890e83245a325245713b6e836b5afbeef2905e
NickSettler <moiseevnikita14@gmail.com>
2019-10-09 21:57:01 +0500

    поменял название переменной. добавил кодировку в файлы

--- Module3/index.php
+++ Module3/index.php
@@ -6,11 +6,11 @@
 	if ($urlArray[0]=="upload") {
 		$name = $_FILES['file']['name'];
 		$explode_img = explode('.', $name);
-		$expansion = $explode_img[1];
+		$extension = $explode_img[1];
 		$tmp_name = $_FILES['file']['tmp_name'];
 		$photos = scandir('photos');
 		$count_photos = count($photos) - 1;
-		move_uploaded_file($tmp_name, 'photos/Untitled'.$count_photos.'.'.$expansion);
+		move_uploaded_file($tmp_name, 'photos/Untitled'.$count_photos.'.'.$extension);
 		$photos = scandir('photos');
 		echo json_encode($photos);
 	}
